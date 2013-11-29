14f8fb4ecdf18c7cfe0c76a3cb5290a4bfcdb7eb
Ömer Özkan <omer@ozkan.info>
2013-11-29 06:47:44 +0200

    refs #35 Administrator domain sınıfı entitiy haline getirildi

--- vipera/vipera-domain/src/main/java/info/ozkan/vipera/entities/Administrator.java
+++ vipera/vipera-domain/src/main/java/info/ozkan/vipera/entities/Administrator.java
@@ -1,7 +1,22 @@
 package info.ozkan.vipera.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ADMINISTRATORS")
 public class Administrator {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
+	private Long id;
+	@Column(name="username", unique=true)
 	private String username;
+	@Column(name="password")
 	private String password;
 	
 	public String getUsername() {
