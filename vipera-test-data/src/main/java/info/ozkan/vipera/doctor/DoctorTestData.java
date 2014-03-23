b92fa53fb8cdcd164949cf07c465b20d2b54add0
Ömer Özkan <omer@ozkan.info>
2014-03-23 09:47:42 +0200

    refs #36 Test verileri için vipera-test-data projesi oluşturuldu DoctorTestData sınıfı buraya taşındı

---
+++ vipera-test-data/src/main/java/info/ozkan/vipera/doctor/DoctorTestData.java
@@ -0,0 +1,29 @@
+package info.ozkan.vipera.doctor;
+
+import info.ozkan.vipera.entities.Doctor;
+import info.ozkan.vipera.entities.DoctorTitle;
+
+/**
+ * Hekimlerle ilgili test verileri
+ * 
+ * @author Ömer Özkan
+ * 
+ */
+public class DoctorTestData {
+
+	public static Doctor getTestData() {
+		final Doctor doctor = new Doctor();
+		doctor.setTckn(12345678901L);
+		doctor.setPassword("password");
+		doctor.setEmail("doctor@doctor.com");
+		doctor.setName("Gregory");
+		doctor.setSurname("House");
+		doctor.setTitle(DoctorTitle.SPECIALIST.getTitle());
+		doctor.setDiplomaNo("12345");
+		doctor.setProvince("Bulaşıcı Hastalıklar ve Nefroloji");
+		doctor.setWebpage("http://www.greghouse.com");
+		doctor.setPhone("+905555555");
+		doctor.setMobilePhone("+905553333333");
+		return doctor;
+	}
+}
