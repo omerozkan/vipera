9583444485f994b59675c9d7de67e86f256925a0
Ömer Özkan <omer@ozkan.info>
2014-04-21 14:04:08 +0300

    refs #134 test derleme hataları düzeltildi

--- vipera/src/test/java/info/ozkan/vipera/business/doctor/DoctorManagerIntegrationTest.java
+++ vipera/src/test/java/info/ozkan/vipera/business/doctor/DoctorManagerIntegrationTest.java
@@ -19,41 +19,42 @@ import org.junit.Test;
  * 
  */
 public class DoctorManagerIntegrationTest extends IntegrationTest {
-	@Inject
-	private DoctorManager doctorManager;
+    @Inject
+    private DoctorManager doctorManager;
 
-	/**
-	 * Sisteme hekim ekler, ekleme işlemi başarılıdır. Veritabanından TCKN ile
-	 * eklenen doktor sorgulanır.
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void addDoctor() throws Exception {
-		final Doctor doctor = DoctorTestData.getTestData();
-		doctor.setTckn(1111111111l);
-		final DoctorManagerResult result = doctorManager.add(doctor);
-		assertTrue(result.isSuccess());
-		final DoctorManagerResult getResult = doctorManager.get(doctor
-		        .getTckn());
-		assertEquals(doctor, getResult.getDoctor());
-	}
+    /**
+     * Sisteme hekim ekler, ekleme işlemi başarılıdır. Veritabanından TCKN ile
+     * eklenen doktor sorgulanır.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void addDoctor() throws Exception {
+        final Doctor doctor = DoctorTestData.getTestData(DoctorTestData.HOUSE);
+        doctor.setTckn(1111111111l);
+        final DoctorManagerResult result = doctorManager.add(doctor);
+        assertTrue(result.isSuccess());
+        final DoctorManagerResult getResult = doctorManager.get(doctor
+                .getTckn());
+        assertEquals(doctor, getResult.getDoctor());
+    }
 
-	/**
-	 * Ünvanı Uzman Doktor olan hekimler için arama işlemi yapılır Gregory House
-	 * ve Ismail Demirci sonuç listesinde yer alır
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void getDoctorsByTitle() throws Exception {
-		final Doctor house = DoctorTestData.getTestData();
-		final Doctor demirci = DoctorTestData.getTestData2();
-		final DoctorBrowseModel model = new DoctorBrowseModel();
-		model.setTitle(DoctorTitle.SPECIALIST);
-		final DoctorManagerResult result = doctorManager.search(model);
-		assertEquals(2, result.getDoctors().size());
-		assertTrue(result.getDoctors().contains(house));
-		assertTrue(result.getDoctors().contains(demirci));
-	}
+    /**
+     * Ünvanı Uzman Doktor olan hekimler için arama işlemi yapılır Gregory House
+     * ve Ismail Demirci sonuç listesinde yer alır
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void getDoctorsByTitle() throws Exception {
+        final Doctor house = DoctorTestData.getTestData(DoctorTestData.HOUSE);
+        final Doctor demirci = DoctorTestData
+                .getTestData(DoctorTestData.DEMIRCI);
+        final DoctorBrowseModel model = new DoctorBrowseModel();
+        model.setTitle(DoctorTitle.SPECIALIST);
+        final DoctorManagerResult result = doctorManager.search(model);
+        assertEquals(2, result.getDoctors().size());
+        assertTrue(result.getDoctors().contains(house));
+        assertTrue(result.getDoctors().contains(demirci));
+    }
 }
