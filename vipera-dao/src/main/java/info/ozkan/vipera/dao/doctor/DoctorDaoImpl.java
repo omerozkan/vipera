d754128b9c9eb56824e215f363cc098ac4b2d960
Ömer Özkan <omer@ozkan.info>
2014-05-04 12:38:54 +0300

    DoctorDaoImpl#find metodu refactor edildi

--- vipera-dao/src/main/java/info/ozkan/vipera/dao/doctor/DoctorDaoImpl.java
+++ vipera-dao/src/main/java/info/ozkan/vipera/dao/doctor/DoctorDaoImpl.java
@@ -45,13 +45,61 @@ public class DoctorDaoImpl implements DoctorDao {
     }
 
     /**
-     * Persistence nesne
+     * {@link DoctorBrowseFilter} nesnesini {@link CriteriaQuery} nesnesine
+     * dönüştürür
      * 
-     * @param em
+     * @param filter
+     *            filtre
+     * @return
      */
-    @PersistenceContext
-    public void setEntityManager(final EntityManager em) {
-        this.em = em;
+    private CriteriaQuery<Doctor> creataCriteriaQueryFromFilter(
+            final DoctorBrowseFilter filter) {
+        final CriteriaBuilder cb = em.getCriteriaBuilder();
+        final CriteriaQuery<Doctor> cq = cb.createQuery(Doctor.class);
+        final Root<Doctor> root = cq.from(Doctor.class);
+        final Map<String, Object> filters = filter.getFilters();
+        final List<Predicate> predicates = new ArrayList<Predicate>();
+        for (final String attr : filters.keySet()) {
+            final Object obj = filters.get(attr);
+            if (obj != null && !obj.toString().isEmpty()) {
+                final String pattern = '%' + obj.toString() + '%';
+                predicates.add(cb.like(
+                        root.<String> get(attr).as(String.class), pattern));
+            }
+        }
+        cq.select(root).where(predicates.toArray(new Predicate[0]));
+        return cq;
+    }
+
+    public DoctorDaoResult delete(final Doctor doctor) {
+        final DoctorDaoResult result = new DoctorDaoResult();
+        em.remove(em.merge(doctor));
+        result.setSuccess(true);
+        result.setDoctor(doctor);
+        return result;
+    }
+
+    /**
+     * Filtrelere göre veritabanında arama işlemi yapar
+     */
+    public List<Doctor> find(final DoctorBrowseFilter filter) {
+        final CriteriaQuery<Doctor> cq = creataCriteriaQueryFromFilter(filter);
+        return em.createQuery(cq).getResultList();
+    }
+
+    public DoctorDaoResult getById(final Long id) {
+        final DoctorDaoResult result = new DoctorDaoResult();
+        final Query query = em.createQuery(JQL_GET_BY_ID);
+        query.setParameter(Doctor.ID, id);
+        final List<Doctor> doctors = query.getResultList();
+        if (doctors.size() == 0) {
+            result.setSuccess(false);
+            result.setError(DoctorManagerError.DOCTOR_NOT_EXIST);
+        } else {
+            result.setSuccess(true);
+            result.setDoctor(doctors.get(0));
+        }
+        return result;
     }
 
     /**
@@ -77,52 +125,18 @@ public class DoctorDaoImpl implements DoctorDao {
     }
 
     /**
-     * Filtrelere göre veritabanında arama işlemi yapar
+     * Persistence nesne
+     * 
+     * @param em
      */
-    public List<Doctor> find(final DoctorBrowseFilter filter) {
-        final CriteriaBuilder cb = em.getCriteriaBuilder();
-        final CriteriaQuery<Doctor> cq = cb.createQuery(Doctor.class);
-        final Root<Doctor> root = cq.from(Doctor.class);
-        final Map<String, Object> filters = filter.getFilters();
-        final List<Predicate> predicates = new ArrayList<Predicate>();
-        for (final String attr : filters.keySet()) {
-            final Object obj = filters.get(attr);
-            if (obj != null && !obj.toString().isEmpty()) {
-                final String pattern = '%' + obj.toString() + '%';
-                predicates.add(cb.like(
-                        root.<String> get(attr).as(String.class), pattern));
-            }
-        }
-        cq.select(root).where(predicates.toArray(new Predicate[0]));
-        return em.createQuery(cq).getResultList();
-    }
-
-    public DoctorDaoResult getById(final Long id) {
-        final DoctorDaoResult result = new DoctorDaoResult();
-        final Query query = em.createQuery(JQL_GET_BY_ID);
-        query.setParameter(Doctor.ID, id);
-        final List<Doctor> doctors = query.getResultList();
-        if (doctors.size() == 0) {
-            result.setSuccess(false);
-            result.setError(DoctorManagerError.DOCTOR_NOT_EXIST);
-        } else {
-            result.setSuccess(true);
-            result.setDoctor(doctors.get(0));
-        }
-        return result;
+    @PersistenceContext
+    public void setEntityManager(final EntityManager em) {
+        this.em = em;
     }
 
     public DoctorDaoResult update(final Doctor doctor) {
         final DoctorDaoResult result = new DoctorDaoResult();
         em.merge(doctor);
-        result.setSuccess(true);
-        result.setDoctor(doctor);
-        return result;
-    }
-
-    public DoctorDaoResult delete(final Doctor doctor) {
-        final DoctorDaoResult result = new DoctorDaoResult();
-        em.remove(em.merge(doctor));
         result.setSuccess(true);
         result.setDoctor(doctor);
         return result;
